a34afa5c4ce4332e856776b274603213486168b7
j-dax <67487509+j-dax@users.noreply.github.com>
2021-07-29 09:33:23 -0700

    updated imports to src. instead of .

--- src/route/caliber.py
+++ src/route/caliber.py
@@ -1,6 +1,6 @@
 from flask import make_response, request
-from config.flask_config import app
-from util.fetch import fetch_json
+from src.config.flask_config import app
+from src.util.fetch import fetch_json
 
 URL_BASE = "https://caliber2-mock.revaturelabs.com:443/mock/"
 
