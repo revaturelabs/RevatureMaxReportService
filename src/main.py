728e9b54697f32c3f2e0b294f0645e69230bfc3b
aboudan98 <amr.aboudan@gmail.com>
2021-07-30 10:32:30 -0500

    fixed a few things with the endpoint

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 # routes need to be touched by import once to proc the app import/setup
 from src.route import caliber
+from src.route.trainer_associate_grades_view import getTraineeData
 from src.config.flask_config import app
 
 from dotenv import dotenv_values
--- src/route/trainer_associate_grades_view.py
+++ src/route/trainer_associate_grades_view.py
@@ -4,8 +4,8 @@ from src.util.fetch import fetch_json
 
 URL_BASE = "https://caliber2-mock.revaturelabs.com:443/mock/"
 
-@app.route("/grades/reports/individual/<associate_email>", methods=["GET"])
-def getTraineeGradesComparedToBatch(associate_email):
-    # localhost:5000/grades/reports/individual/mock8.associatef4c8d0c5-ecaf-4127-a459-7bf3617118a6@mock.com
+@app.route("/trainer/individual/grades/<associate_email>", methods=["GET"])
+def getTraineeData(associate_email):
+    # localhost:5000/trainer/individual/grades/mock8.associatef4c8d0c5-ecaf-4127-a459-7bf3617118a6@mock.com
     url = f"{URL_BASE}evaluation/grades/reports/individual/{associate_email}"
-    return {"batch_grades": fetch_json(url)}
+    return {"associateGrades": fetch_json(url)["traineeGrades"]}
