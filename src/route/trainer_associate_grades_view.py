ea4f8c88d79aa9d99000af54a35cb92faef391f1
aboudan98 <amr.aboudan@gmail.com>
2021-08-03 10:49:53 -0500

    added unit test for my route

--- src/route/trainer_associate_grades_view.py
+++ src/route/trainer_associate_grades_view.py
@@ -8,4 +8,4 @@ URL_BASE = "https://caliber2-mock.revaturelabs.com:443/mock/"
 def getTraineeData(associate_email):
     # localhost:5000/trainer/individual/grades/mock8.associatef4c8d0c5-ecaf-4127-a459-7bf3617118a6@mock.com
     url = f"{URL_BASE}evaluation/grades/reports/individual/{associate_email}"
-    return {"associateGrades": fetch_json(url)["traineeGrades"]}
+    return {"associateGrades": fetch_json(url)['traineeGrades']}
