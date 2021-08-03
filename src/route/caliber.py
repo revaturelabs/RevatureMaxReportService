404a47933fa24695981017ea4d824bdbd3396e1d
j-dax <67487509+j-dax@users.noreply.github.com>
2021-08-03 07:31:53 -0700

    moved test dir back to project root

--- src/route/caliber.py
+++ src/route/caliber.py
@@ -5,6 +5,36 @@ from src.util.fetch import fetch_json
 URL_BASE = "https://caliber2-mock.revaturelabs.com:443/mock/"
 
 
+@app.route("/associate/<associate_email>")
+def associate_listing(associate_email):
+    # localhost:5000/associate/mock8.associatef4c8d0c5-ecaf-4127-a459-7bf3617118a6@mock.com
+    url = f"{URL_BASE}training/associate/{associate_email}"
+    batches = fetch_json(url)
+    return {**batches}, 200
+
+
+@app.route("/associate/<associate_email>/batch")
+def associate_batch_listing(associate_email):
+    # localhost:5000/associate/mock8.associatef4c8d0c5-ecaf-4127-a459-7bf3617118a6@mock.com/batch
+    url = f"{URL_BASE}training/associate/{associate_email}/batch"
+    batches = fetch_json(url)
+    return {**batches}, 200
+
+
+@app.route("/grades/reports/individual/<associate_email>", methods=["GET"])
+def get_trainee_grades_compared_to_batch(associate_email):
+    # localhost:5000/grades/reports/individual/mock8.associatef4c8d0c5-ecaf-4127-a459-7bf3617118a6@mock.com
+    url = f"{URL_BASE}evaluation/grades/reports/individual/{associate_email}"
+    return {**fetch_json(url)}
+
+
+@app.route("/grades/reports/individual/<associate_email>/<int:week>", methods=["GET"])
+def get_trainee_grades_compared_to_batch_on_week(associate_email, week):
+    # localhost:5000/grades/reports/individual/mock8.associatef4c8d0c5-ecaf-4127-a459-7bf3617118a6@mock.com/2
+    url = f"{URL_BASE}evaluation/grades/reports/individual/{associate_email}/{week}"
+    return {**fetch_json(url)}
+
+
 @app.route("/grades/assessment/<int:assessment_id>", methods=["GET"])
 def findGradesByAssessment(assessment_id):
     # localhost:5000/grades/assessment/2169
--- src/route/self_comparison.py
+++ src/route/self_comparison.py
@@ -1,40 +1,8 @@
 # from flask import make_response, request
-from src.config.loggingConfig import controller_log as logger
 from src.config.flask_config import app
-from src.util.fetch import fetch_json
 from src.util.self_comparison import individual_vs_batch_score, score_report_by_week
 
 URL_BASE = "https://caliber2-mock.revaturelabs.com:443/mock/"
-
-
-@app.route("/associate/<associate_email>")
-def associate_listing(associate_email):
-    # localhost:5000/associate/mock8.associatef4c8d0c5-ecaf-4127-a459-7bf3617118a6@mock.com
-    url = f"{URL_BASE}training/associate/{associate_email}"
-    batches = fetch_json(url)
-    return {**batches}, 200
-
-
-@app.route("/associate/<associate_email>/batch")
-def associate_batch_listing(associate_email):
-    # localhost:5000/associate/mock8.associatef4c8d0c5-ecaf-4127-a459-7bf3617118a6@mock.com/batch
-    url = f"{URL_BASE}training/associate/{associate_email}/batch"
-    batches = fetch_json(url)
-    return {**batches}, 200
-
-
-@app.route("/grades/reports/individual/<associate_email>", methods=["GET"])
-def get_trainee_grades_compared_to_batch(associate_email):
-    # localhost:5000/grades/reports/individual/mock8.associatef4c8d0c5-ecaf-4127-a459-7bf3617118a6@mock.com
-    url = f"{URL_BASE}evaluation/grades/reports/individual/{associate_email}"
-    return {**fetch_json(url)}
-
-
-@app.route("/grades/reports/individual/<associate_email>/<int:week>", methods=["GET"])
-def get_trainee_grades_compared_to_batch_on_week(associate_email, week):
-    # localhost:5000/grades/reports/individual/mock8.associatef4c8d0c5-ecaf-4127-a459-7bf3617118a6@mock.com/2
-    url = f"{URL_BASE}evaluation/grades/reports/individual/{associate_email}/{week}"
-    return {**fetch_json(url)}
 
 
 @app.route("/reports/<batch_id>/<associate_email>/weekly", methods=["GET"])
