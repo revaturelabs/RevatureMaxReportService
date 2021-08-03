61b2a6fe847e298a82b811b73e600de5325cce81
j-dax <67487509+j-dax@users.noreply.github.com>
2021-08-03 08:06:49 -0700

    ff

---
+++ src/route/self_comparison.py
@@ -0,0 +1,29 @@
+# from flask import make_response, request
+from src.config.flask_config import app
+from src.util.self_comparison import (
+    individual_vs_batch_score,
+    individual_score_by_week,
+    batch_averages_by_week,
+)
+
+URL_BASE = "https://caliber2-mock.revaturelabs.com:443/mock/"
+
+
+@app.route("/reports/<batch_id>/<associate_email>/weekly", methods=["GET"])
+def get_trainee_grades_compared_to_batch_all_weeks(batch_id, associate_email):
+    # localhost:5000/reports/TR-1190/mock8.associatef4c8d0c5-ecaf-4127-a459-7bf3617118a6@mock.com/weekly
+    return individual_vs_batch_score(batch_id, associate_email)
+
+
+@app.route("/reports/<batch_id>/spider/<associate_email>", methods=["GET"])
+def spider_on_batch(batch_id, associate_email):
+    # localhost:5000/reports/TR-1190/spider/mock8.associatef4c8d0c5-ecaf-4127-a459-7bf3617118a6@mock.com
+    results = individual_score_by_week(batch_id, associate_email)
+    batch_scores = batch_averages_by_week(batch_id)
+
+    results["data"]["Batch Averages"] = batch_scores["data"]["Batch Averages"]
+    results["chartData"]["Batch Average Score"] = batch_scores["chartData"][
+        "Batch Average Score"
+    ]
+
+    return results
