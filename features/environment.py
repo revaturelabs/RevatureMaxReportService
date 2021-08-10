a8bd0b583266c7df687f074fe878637ae0e6e1c1
aboudan98 <amr.aboudan@gmail.com>
2021-08-10 10:31:58 -0500

    added feature directory

---
+++ features/environment.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+
+def before_all(context):
+    context.driver = webdriver.Chrome(executable_path='/Users/amrab/Downloads/chromedriver_win32/chromedriver')
+
+
+def after_all(context):
+    context.driver.quit()
