ec8cca620ba2d8281a415f5a9edf2f49cf1206ad
aboudan98 <amr.aboudan@gmail.com>
2021-08-10 13:16:53 -0500

    added selenium file structure

---
+++ features/change_password.feature
@@ -0,0 +1,3 @@
+Feature: Change password
+
+  Scenario: A user is on the password change page and 
--- features/environment.py
+++ features/environment.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 
+
 def before_all(context):
     context.driver = webdriver.Chrome(executable_path='/Users/amrab/Downloads/chromedriver_win32/chromedriver')
 
